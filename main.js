ba2594e0d55ab37a4571e57ec732c0b6f16f81dd
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2021-08-18 10:31:18 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400', true);
+//request.respondType = 'json';
+//request.send();
+
+request.onload = function() {
+  //const riseSet = request.response;
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400){
+    data.forEach((results) => {
+      console.log(results.sunrise)
+    })
+  } else {
+    console.log('error');
+  }
+}
+
+request.send();
+
+
